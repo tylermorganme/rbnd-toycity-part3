20dda051c6c535bd80890a701008ba222ae86365
Tyler <tyler@tylermorgan.me>
2016-01-07 23:55:10 -0800

    Added errors for duplicate products

--- lib/product.rb
+++ lib/product.rb
@@ -1,7 +1,16 @@
 class Product
+  attr_reader :title, :price, :stock
+
   @@products = []
   def initialize(options={})
-    @@product << self
+    if (@@products.any? {|product| product.title == options[:title]})
+      raise DuplicateProductError, "'#{options[:title]}' already exists."
+    else
+      @title = options[:title]
+      @price = options[:price]
+      @stock = options[:stock]
+      @@products << self
+    end
   end
   def self.all
     @@products

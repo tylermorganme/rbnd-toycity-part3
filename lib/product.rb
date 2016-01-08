88a8451e5ecd5008645b9a856eeb385b1d39ccd7
Tyler <tyler@tylermorgan.me>
2016-01-08 00:08:22 -0800

    Add find_by_title, in_stock, and in_stock? to Product

--- lib/product.rb
+++ lib/product.rb
@@ -2,6 +2,7 @@ class Product
   attr_reader :title, :price, :stock
 
   @@products = []
+
   def initialize(options={})
     if (@@products.any? {|product| product.title == options[:title]})
       raise DuplicateProductError, "'#{options[:title]}' already exists."
@@ -12,7 +13,21 @@ class Product
       @@products << self
     end
   end
+
   def self.all
     @@products
   end
+
+  def self.find_by_title(title)
+    @@products.find {|product| product.title == title}
+  end
+
+  def self.in_stock
+    @@products.find_all {|product| product.in_stock?}
+  end
+
+  def in_stock?
+    @stock > 0
+  end
+
 end
